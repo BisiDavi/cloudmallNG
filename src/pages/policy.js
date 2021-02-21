bc3a7b92af53f07e31c2de2058df92bf790e80cf
BisiDavi <oludavidconnect@gmail.com>
2021-02-21 12:05:58 +0100

    pages added

---
+++ src/pages/policy.js
@@ -0,0 +1,40 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import { Pageheader } from '../imports';
+
+const displayPolicy = () => {
+  let PolicyArray = [];
+  const terms =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci at egestas etiam et leo arcu. Mattis pharetra, augue ullamcorper in.';
+
+  for (let i = 0; i <= 4; i++) {
+    PolicyArray.push(`Policy ${i + 1}.`);
+  }
+
+  console.log('PolicyArray', PolicyArray);
+
+  return PolicyArray.map((policy, index) =>
+    index === 4 ? (
+      <Col  key={policy} lg={12} sm={12}>
+        <h4>{policy}</h4>
+        <p>A. {terms}</p>
+        <p>B. {terms}</p>
+      </Col>
+    ) : (
+      <Col key={policy} lg={12} sm={12}>
+        <h4>{policy}</h4>
+        <p>{terms}</p>
+      </Col>
+    )
+  );
+};
+
+const PrivacyPolicy = () => {
+  return (
+    <Container className="privacyPolicy">
+      <Pageheader title=" Privacy Policy" />
+      <Row className="pt-3">{displayPolicy()}</Row>
+    </Container>
+  );
+};
+
+export default PrivacyPolicy;
